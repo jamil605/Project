4722f5c89c19adaa2389e40a70f907a9d010671e
jamil605 <ShehabJamil201@gmail.com>
2021-05-11 23:04:34 +0300

    files and folders are added in app folder

---
+++ app/Http/Controllers/DashBoardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class DashBoardController extends Controller
+{
+   
+     public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+        return view('dashboard')->with('posts', $user->posts);
+    }
+}
